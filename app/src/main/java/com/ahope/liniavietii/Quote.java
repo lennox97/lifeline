ff5f1e0209f44489aafc2628082f9742a9575aab
lennox97 <lennox.samp@yahoo.com>
2018-07-28 23:04:10 +0300

    Quote class done

---
+++ app/src/main/java/com/ahope/liniavietii/Quote.java
@@ -0,0 +1,21 @@
+package com.ahope.liniavietii;
+
+public class Quote {
+    public String title;
+    public String year;
+    public String quote;
+    public String chapter;
+    public String vers;
+
+    public Quote() {
+        
+    }
+
+    public Quote(String title, String year, String quote, String chapter, String vers) {
+        this.title = title;
+        this.year = year;
+        this.quote = quote;
+        this.chapter = chapter;
+        this.vers = vers;
+    }
+}
